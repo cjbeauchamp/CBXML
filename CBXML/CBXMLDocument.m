4aca4c058cdbf3ecc8b4ea6340fa174e59192ad3
Chris Beauchamp <cjbeauchamp@gmail.com>
2013-04-23 22:54:57 -0700

    removed logging

--- CBXML/CBXMLDocument.m
+++ CBXML/CBXMLDocument.m
@@ -40,40 +40,30 @@ didStartElement:(NSString *)elementName
  qualifiedName:(NSString *)qualifiedName
     attributes:(NSDictionary *)attributeDict
 {
-    NSLog(@"ElementName: %@ in uri: %@ withQName: %@ andDict: %@", elementName, namespaceURI, qualifiedName, attributeDict);
-    
     // create a new node in our document
     CBXMLNode *current = [CBXMLNode nodeWithName:elementName andAttributes:attributeDict];
     [_openNodes addObject:current];
 }
 
 - (void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string {
-    NSLog(@"Found chars: %@", string);
     [_captured appendString:string];
-    NSLog(@"Got 'em: %@", _captured);
 }
 
 - (void)parser:(NSXMLParser *)parser didEndElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName {
     
     // trim our string
-    NSLog(@"pre-trim: '%@'", _captured);
     _captured = [[_captured stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] mutableCopy];
-    NSLog(@"post-trim: '%@'", _captured);
 
     if([_captured length] == 0) {
         _captured = nil;
     }
-    
-    NSLog(@"CAPTURED: '%@'", _captured);
-    
+        
     CBXMLNode *current = [_openNodes lastObject];
     
     if(_captured != nil) {
         [current setNodeValue:[NSString stringWithString:_captured]];        
     }
-    
-    NSLog(@"CAPTURED_POST: %@", current);
-    
+        
     // add this to its parent (or root if none)
     if([_openNodes count] > 1) {
         [(CBXMLNode*)[_openNodes objectAtIndex:_openNodes.count-2] addChild:current];
@@ -81,9 +71,6 @@ didStartElement:(NSString *)elementName
         [_children addObject:current];
     }
     
-    NSLog(@"Ended element: %@", current);
-
-    // TODO: add this node to the document (or open element)
     [_openNodes removeLastObject];
     
     // clear our captured chars
